eb8b53b058f1cd42f561a53309a5a5e10d01fba0
Yomaira Escano <yjescano@gmail.com>
2018-03-08 04:32:02 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,11 @@
 class StudentsController < ApplicationController
 
-  def new
-    @student = Student.new
-  end
-
   def show
     @student = Student.find(params[:id])
+  end
+
+  def new
+    @student = Student.new
   end
 
   def create
