a3b60916674ac4cb43008c5b4479e26deecad78f
Yomaira Escano <yjescano@gmail.com>
2018-03-08 03:03:00 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,14 +1,16 @@
 class SchoolClassesController < ApplicationController
 
   def create
-    @school_class = SchoolClass.(post_params())
+    @school_class = SchoolClass.(school_class_params(:title, :room_number))
+    @school_class.save
+    redirect_to school_class_path(@school_class)
   end
 
 
 private
 
-  def student_class_params(*args)
-    params.require(:student_class).permit(*args)
+  def school_class_params(*args)
+    params.require(:school_class).permit(*args)
   end
 
 end
